858842c7165ae6bce8e0ff843575a944d57e4980
Damilare Tolulope <damilaretolulope1@gmail.com>
2024-10-28 21:13:53 +0100

    TailwindCSS class

---
+++ src/script/newScript.js
@@ -0,0 +1,18 @@
+console.log("Hello World!!")
+
+// id
+const firstElement = document.getElementById('first');
+firstElement.style.fontSize = "40px";
+
+const secElement = document.getElementById('text');
+secElement.style.fontWeight= "bold";
+secElement.style.color = "blue"
+
+// className
+
+// tagName
+// querySelector
+document.querySelector("#first");
+
+// querySelectorAll
+document.querySelectorAll("")
